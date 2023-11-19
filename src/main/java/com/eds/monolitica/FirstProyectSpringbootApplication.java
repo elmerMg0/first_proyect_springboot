56ad0c093d4f44b16eeba0176490adef29dade0a
elmerMg0 <mendozagutierrezelmer20@gmail.com>
2023-11-19 11:22:25 -0400

    first commit

---
+++ src/main/java/com/eds/monolitica/FirstProyectSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.eds.monolitica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProyectSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProyectSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eds/monolitica/FirstProyectSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eds.monolitica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProyectSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
